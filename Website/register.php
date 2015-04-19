80fd6e068a18f683ce24fc24469a27682f84407e
anbe6083 <anbe6083@colorado.edu>
2015-04-19 17:50:28 -0600

    Andrew: Updated register.php file

--- Website/register.php
+++ Website/register.php
@@ -85,51 +85,8 @@
         
 
         $user = $_POST['username']; //pull the username from the username field
-        $user_path= "/home/buffbox/uploads/{$user}";
+        $user_path= "/home/pi/Uploads/{$user}";
         mkdir($userpath, 0777, true);
-$ftp_server= "ftp://172.21.64.158";
-$ftp_user_name= 'pi';
-$ftp_user_pass = 'raspberry';
-$destination_file= " /home/pi/Desktop";
-$source_file= $user_path;
-      // set up basic connection
-$conn_id = ftp_connect($ftp_server, 20); 
-
-// login with username and password
-$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass); 
-
-// check connection
-if ((!$conn_id) || (!$login_result)) { 
-    if(!$conn_id){
-
-        echo "{$conn_id}";
-        echo "connection id failed\n";
-    }
-    else {
-
-        echo "login_result failed\n";
-    }
-    echo "FTP connection has failed!";
-    echo "Attempted to connect to $ftp_server for user $ftp_user_name"; 
-    exit; 
-} else {
-    echo "Connected to $ftp_server, for user $ftp_user_name";
-}
-
-// upload the file
-$upload = ftp_put($conn_id, $destination_file, $source_file, FTP_BINARY); 
-
-// check upload status
-if (!$upload) { 
-    echo "FTP upload has failed!";
-} else {
-    echo "Uploaded $source_file to $ftp_server as $destination_file";
-}
-
-// close the FTP stream 
-ftp_close($conn_id);
-
-
         header("Location: index.php"); 
         die("Redirecting to index.php"); 
 
