d91874f01deff860b561d1dd86e1ec409d8a8edc
anbe6083 <anbe6083@colorado.edu>
2015-04-19 18:49:55 -0600

    Andrew: Edited config.php dbname to phpmyadmin

--- Website/config.php
+++ Website/config.php
@@ -4,7 +4,7 @@
     $username = "stepup2stepout"; 
     $password = "Liris72259"; 
     $host = "localhost"; 
-    $dbname = "Login"; 
+    $dbname = "phpmyadmin"; 
     
     $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'); 
     try { $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options); } 
