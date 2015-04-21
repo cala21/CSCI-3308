fc9a8b8ec5a26e3b39f9b32ee607af39dd26cf51
anbe6083 <anbe6083@colorado.edu>
2015-04-20 18:40:08 -0600

    Andrew: Updated register.php to mkdir to /home/pi/{}

--- Website/register.php
+++ Website/register.php
@@ -91,7 +91,7 @@
         $user = $_POST['username']; //pull the username from the username field
         $user_path= "/home/pi/Uploads/{$user}";
         mkdir($user_path, 0777);
-        if(mkdir("/home/pi/Uploads/{$user}";, 0777)){
+        if(mkdir("/home/pi/Uploads/{$user}", 0777)){
 
             echo "Directory Created";
         }
